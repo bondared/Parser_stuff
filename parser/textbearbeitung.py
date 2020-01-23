1ef117bfde86944dfd904bc594227aca82e5c8ad
Marco Lehne <lehnemar@hu-berlin.de>
2020-01-23 16:16:15 +0100

    textbearbeitung

---
+++ parser/textbearbeitung.py
@@ -0,0 +1,16 @@
+def ersetzen(was,datei):
+
+    import re
+        
+    alte_datei = open(datei+'.txt','r')
+    neue_datei = open(datei+'_bearbeitet.txt','w')
+    
+    for line in alte_datei:
+        new_line = re.sub(was, '', line)
+        neue_datei.write(new_line)
+    
+    neue_datei.close()
+        
+  
+
+ersetzen(' . ','text')
